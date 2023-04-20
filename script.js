610f77aa2bc0fa97c02e740a4ad4e02b00cbff6d
teresarey8 <123724097+teresarey8@users.noreply.github.com>
2023-04-20 11:03:52 +0200

    alerts

--- script.js
+++ script.js
@@ -12,21 +12,26 @@ function calcular() {
 function sumar(num1, num2) {
     let resultado = num1 + num2;
     console.log(`La suma es: ${resultado}`);
+    alert("El resultado de la suma es: " + suma);
 }
 
 
 function restar(num1, num2) {
     let resultado = num1 - num2;
     console.log(`La resta es: ${resultado}`);
+    alert("El resultado de la resta es: " + resta);
 }
 
 function multiplicar(num1, num2) {
     let resultado = num1 * num2;
     console.log(`La multiplicación es: ${resultado}`);
+    alert("El resultado de la multiplicación es: " + multiplicación);
 }
 
 
 function dividir(num1, num2) {
     let resultado = num1 / num2;
     console.log(`La división es: ${resultado}`);
+    alert("El resultado de la división es: " + división);
+
 }
